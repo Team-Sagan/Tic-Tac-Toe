0cbee770f3cd319fafd284a12471e63e9e037620
ViktorGorchev <viktor_g_g@yahoo.com>
2016-03-06 21:52:08 +0200

    Computer player logic Implementation. /adding class HumanPlayer.cs/

---
+++ Tic_Tack_Toe/Models/HumanPlayer.cs
@@ -0,0 +1,12 @@
+﻿namespace Tic_Tack_Toe.Models
+{
+    using Tic_Tack_Toe.Interfaces;
+
+    public class HumanPlayer : Player
+    {
+        public HumanPlayer(string playerName, string playerSymbol)
+            : base(playerName, playerSymbol)
+        {
+        }
+    }
+}
