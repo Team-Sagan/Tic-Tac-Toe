f859e1c706aa9c52ef8d8309c293d4459172343f
redis1966 <programmaker@abv.bg>
2016-03-09 02:50:04 +0200

    Main menu and icon added
    Second form needed + logic

---
+++ Tic_Tack_Toe/Menu.cs
@@ -0,0 +1,17 @@
+﻿namespace Tic_Tack_Toe
+{
+    using System.Windows.Forms;
+
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, System.EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
